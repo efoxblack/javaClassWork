d819b16d5f3251695628d795c7d6d916249221e4
ErikFox <erikmichaelfox@gmail.com>
2020-04-10 16:30:36 -0400

    Update loop class

--- src/main/java/loops/Main.java
+++ src/main/java/loops/Main.java
@@ -57,23 +57,33 @@ public class Main {
 //        System.out.println(main.printArray(breakfast));
 //    }
 
-    public String printArray(String[] input){
-        String response = "*** Output ***";
-        //DO NOT TYPE ABOVE HERE!!!
+//    public String printArray(String[] input){
+//        String response = "*** Output ***";
+//        //DO NOT TYPE ABOVE HERE!!!
         // Place your code here
 //        String [] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
-        for (int i = 0; i < input.length; i++) {
-            String s = input[i];
-            response = response + "\n" + s;
-        }
-        return response;
+//        for (int i = 0; i < input.length; i++) {
+//            String s = input[i];
+//            response = response + "\n" + s;
+//        }
+//        return response;
+//    }
+//    //DO NOT TYPE ABOVE HERE!!!
+//    /** DO NOT EDIT THIS CODE **/
+//    public static void main(String[] args){
+//        Main main = new Main();
+//        String [] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+//        System.out.println(main.printArray(breakfast));
+//    }
+
+    public static String printGivenStringTimesNumberGiven(String strongarm, int nancy) {
+        //Type your code here
+        String repeat = new String (new char[nancy]).replace("\0", strongarm);
+        return repeat;
     }
-    //DO NOT TYPE ABOVE HERE!!!
-    /** DO NOT EDIT THIS CODE **/
+    //DO NOT TYPE BELOW HERE
     public static void main(String[] args){
-        Main main = new Main();
-        String [] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
-        System.out.println(main.printArray(breakfast));
+        System.out.println(printGivenStringTimesNumberGiven("ha", 3));
     }
 }
 
